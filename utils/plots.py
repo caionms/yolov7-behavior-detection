0cebc76938893e0e1c4b4e06b8b8e647a985c1f4
Caio Nery <caionms@ufba.br>
2023-06-15 18:36:53 -0300

    Adicionando color_list no plot.utils

--- utils/plots.py
+++ utils/plots.py
@@ -519,3 +519,10 @@ def plot_results(start=0, stop=0, bucket='', id=(), labels=(), save_dir=''):
 
     ax[1].legend()
     fig.savefig(Path(save_dir) / 'results.png', dpi=200)
+    
+def color_list():
+    # Return first 10 plt colors as (r,g,b) https://stackoverflow.com/questions/51350872/python-from-color-name-to-rgb
+    def hex2rgb(h):
+        return tuple(int(h[1 + i:1 + i + 2], 16) for i in (0, 2, 4))
+
+    return [hex2rgb(h) for h in matplotlib.colors.TABLEAU_COLORS.values()]  # or BASE_ (8), CSS4_ (148), XKCD_ (949)
